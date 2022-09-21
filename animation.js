9043df63f399a7ef653cd388707d8a83317494c6
Aventia <lauralis0405@gmail.com>
2022-09-21 17:37:22 +0200

    desktop version 1.0

--- animation.js
+++ animation.js
@@ -1,23 +1,43 @@
 var errorMessage = document.querySelector('.error-message')
 var inputForm = document.querySelector(".email-input");
+var errorIcon = document.querySelector('.error-icon')
+var checked = false;
+var inputText;
 
 function validateEmail() {
-    var inputText = inputForm.value;    
+
+    if (checked == true) {
+        errorMessage.textContent = "";
+        $(inputForm).css({
+            "border": "1px solid hsl(0, 93%, 68%)",
+            "color": "hsl(0, 93%, 68%)"
+        });
+        $(errorIcon).css({
+            "display": "none"
+        })
+    }
+
+    inputText = inputForm.value;    
+
     if (inputText == "") {
         errorMessage.textContent ="Please provide a valid email";
         changeInputStyle();
     } else if (!inputText.includes("@")) {
         errorMessage.textContent ="Please provide a valid email";
         changeInputStyle();
+    } else {
+        alert("Congratulations! You have successfully signed up to our newsletter.");
+        inputForm.value = "Email Address";
     }
 }
 
 function changeInputStyle() {
+    checked = true;
     $(inputForm).css({
         "border": "2px solid hsl(0, 93%, 68%)",
         "color": "hsl(0, 93%, 68%)"
-    }),
-    $(inputForm).children('.error-icon').css({
+    });
+    $(errorIcon).css({
         "display": "inline-block"
     })
 }
