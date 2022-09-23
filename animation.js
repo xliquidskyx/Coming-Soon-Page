3c0ec4137f7cb444c0ebb408fcba1fb2afb37c05
Aventia <lauralis0405@gmail.com>
2022-09-23 18:13:37 +0200

    improved email validation and css

--- animation.js
+++ animation.js
@@ -9,8 +9,8 @@ function validateEmail() {
     if (checked == true) {
         errorMessage.textContent = "";
         $(inputForm).css({
-            "border": "1px solid hsl(0, 93%, 68%)",
-            "color": "hsl(0, 93%, 68%)"
+            "border": "1px solid hsl(0, 36%, 70%)",
+            "color": "hsl(0, 36%, 70%)"
         });
         $(errorIcon).css({
             "display": "none"
@@ -19,15 +19,28 @@ function validateEmail() {
 
     inputText = inputForm.value;    
 
-    if (inputText == "") {
+    if (inputText == "") { //check if the input is empty
         errorMessage.textContent ="Please provide a valid email";
         changeInputStyle();
-    } else if (!inputText.includes("@")) {
+        return;
+    } else if (!inputText.includes("@")) { //check if it includes @
         errorMessage.textContent ="Please provide a valid email";
+        changeInputStyle();
+        return;
+    } 
+
+    const splitEmail = inputText.split("@");
+
+    if (splitEmail[0] == "" || splitEmail[0].length < 2|| splitEmail[1] == "" || splitEmail[1].length < 5) {
+        errorMessage.textContent = "Please provide a valid email"; //check if it has characters before and after @ character
+        changeInputStyle();
+    } else if(!splitEmail[1].includes(".") || splitEmail[1].slice(-1) == ".") {
+        errorMessage.textContent = "Please provide a valid email"; //check if 2nd part of the str has . and its not the last element
         changeInputStyle();
     } else {
         alert("Congratulations! You have successfully signed up to our newsletter.");
-        inputForm.value = "Email Address";
+        inputForm.value = ""; //clears input text
+        inputForm.placeholder = "Email Address"; //changes the placeholder
     }
 }
 
